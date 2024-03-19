df59b2d4bd451af1354ba71273ace088b2c48c59
Bilegtluu Bat-Tumur <23LP3516@G304-CC0043.local>
2024-03-19 10:31:16 +0100

    product model

--- backEnd/models/productModel.ts
+++ backEnd/models/productModel.ts
@@ -1,5 +1,52 @@
-import { Schema, model } from "mongoose";
+import { Schema } from "mongoose";
 
 const productSchema = new Schema({
-  productName: String,
+  productName: {
+    type: String,
+    required: [true, "Please check name"],
+  },
+  categoryId: {
+    type: String,
+    required: [true],
+  },
+  price: {
+    type: String,
+    required: [true, "Please check price"],
+  },
+  qty: {
+    type: Number,
+  },
+  thumbnails: {
+    type: String,
+  },
+  images: {
+    type: String,
+    required: [true, "Please check image"],
+  },
+  coupon: {
+    String,
+  },
+  salePercent: {
+    type: Number,
+  },
+  description: {
+    type: String,
+    required: [true, "Please check description"],
+  },
+  viewsCount: {
+    type: Number,
+    required: [true, "Please check viewsCount"],
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+    required: [true, " Please check createdAt"],
+  },
+  updatedAt: {
+    type: Date,
+    default: new Date(),
+    required: [true, " Please check createdAt"],
+  },
 });
+
+export default productSchema;
