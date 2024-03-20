23a171425f6b20dbc78d21d1e2d684e26c09ae40
Orgil Ariunbileg <0rgilshdee@192.168.1.9>
2024-03-20 22:15:30 +0800

    orgi

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col">
-      <Header />
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
