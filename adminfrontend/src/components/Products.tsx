18e6456bd5829c0d4e642b388d7aeb7400795ffd
Namkhaidorj Batbaatar <mglcoc102@gmail.com>
2024-03-23 14:35:23 +0800

    adminFrontEnd new style

--- adminfrontend/src/components/Products.tsx
+++ adminfrontend/src/components/Products.tsx
@@ -1,9 +1,10 @@
-import { instance } from "@/instance";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
+import { instance } from "@/instance";
+import cloudinary from "cloudinary";
 
 export const Products = () => {
   // ID from edit button=========
@@ -22,37 +23,38 @@ export const Products = () => {
   });
   //=============================
 
-  const [productName, setProductName] = useState("");
-  const [description, setDescription] = useState("");
+  const [bagName, setBagName] = useState("");
+  const [colorId, setColorId] = useState("");
   const [price, setPrice] = useState();
-  const [productNumber, setProductNumber] = useState();
-  const [images, setImages] = useState();
-  const [qty, setQty] = useState();
+  const [brand, setBrand] = useState();
+  const [images, setImages] = useState([]);
+  const [bagType, setBagType] = useState();
+  const [bagCode, setBagCode] = useState();
 
   const addProduct = async (e: any) => {
     try {
       const input = {
-        productName: productName,
-        description: description,
-        price: price,
-        productNumber: productNumber,
-        qty: qty,
+        bagName,
+        colorId,
+        price,
+        brand,
+        bagType,
+        images,
+        bagCode,
       };
+      const imgUlr = url;
+      const imagesUlr = [imgUlr];
 
-      if (
-        !images ||
-        !productName ||
-        !description ||
-        !price ||
-        !productNumber ||
-        !qty
-      )
-        return;
-      console.log("wtf", input);
+      const color = { colorId, imagesUlr };
+
+      const colors = [color];
+
+      if (!images) return;
+      console.log("input", input);
 
       const formData = new FormData();
       formData.append("input", JSON.stringify(input));
-      formData.append("image", images);
+      formData.append("colors", JSON.stringify(colors));
       await instance.post("/productCreate", formData);
 
       console.log("first", formData);
@@ -83,34 +85,34 @@ export const Products = () => {
   }, []);
 
   // Updating product scene =============================================
-  const updateProduct = async () => {
-    try {
-      const input = {
-        name: productName,
-        desc: description,
-        price: price,
-        categoryid: productNumber,
-        qnty: qty,
-        img: images,
-      };
+  // const updateProduct = async () => {
+  //   try {
+  //     const input = {
+  //       name: productName,
+  //       desc: description,
+  //       price: price,
+  //       categoryid: productNumber,
+  //       qnty: qty,
+  //       img: images,
+  //     };
 
-      const response = await axios.put(
-        `http://localhost:8080/productUpdate/${_id}`,
-        input
-      );
-      if (response.status === 200) {
-        alert("Product updated successfully");
-        console.log(response.data);
-      } else {
-        alert("Failed to update product");
-        console.error(response.data);
-      }
-      console.log(response);
-    } catch (error) {
-      alert(error.response.data.message);
-      console.error(error);
-    }
-  };
+  //     const response = await axios.put(
+  //       `http://localhost:8080/productUpdate/${_id}`,
+  //       input
+  //     );
+  //     if (response.status === 200) {
+  //       alert("Product updated successfully");
+  //       console.log(response.data);
+  //     } else {
+  //       alert("Failed to update product");
+  //       console.error(response.data);
+  //     }
+  //     console.log(response);
+  //   } catch (error) {
+  //     alert(error.response.data.message);
+  //     console.error(error);
+  //   }
+  // };
 
   return (
     <div>
@@ -122,20 +124,20 @@ export const Products = () => {
           </Link>
         </div>
         <div className="p-8 flex gap-6">
-          <div className="w-1/2 flex flex-col gap-6">
+          <div className="w-full flex flex-col gap-6">
             <div className="bg-white p-6 rounded-[12px] flex flex-col gap-6">
               <div className="flex flex-col gap-2">
                 <p className="text-sm font-semibold">Бүтээгдэхүүн нэмэх</p>
                 {update ? (
                   <input
-                    onChange={(e) => setProductName(e.currentTarget.value)}
+                    onChange={(e) => setBagName(e.currentTarget.value)}
                     defaultValue={product.productName}
                     className="bg-[#F7F7F8] p-2 rounded-[8px]"
                     type="text"
                   />
                 ) : (
                   <input
-                    onChange={(e) => setProductName(e.target.value)}
+                    onChange={(e) => setBagName(e.target.value)}
                     placeholder="Нэр"
                     className="bg-[#F7F7F8] p-2 rounded-[8px]"
                     type="text"
@@ -161,20 +163,35 @@ export const Products = () => {
                   />
                 )}
               </div>
-
-              <div className="flex flex-col gap-2">
-                <p className="text-sm font-semibold">Барааны код</p>
-                <input
-                  onChange={(e) => setProductNumber(e.target.value)}
-                  placeholder="#12345678"
-                  className="bg-[#F7F7F8] p-2 rounded-[8px]"
-                  type="text"
-                />
-              </div>
             </div>
 
             <div>
-              <div className="h-[231px] p-6 flex flex-col gap-4 border rounded-2xl bg-white">
+              <div className="p-6 flex flex-col gap-4 border rounded-2xl bg-white">
+                <div className="flex flex-col gap-2">
+                  <p>Өнгө</p>
+
+                  <select
+                    className="border-[1px] bg-[#F7F7F8] p-2 rounded-[8px]"
+                    name=""
+                    id="">
+                    <option value=""></option>
+                    <option value="">White & Black</option>
+                    <option value="">Silver</option>
+                    <option value="">Red</option>
+                    <option value="">Biege</option>
+                  </select>
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <p className="text-sm font-semibold">Барааны код</p>
+                  <input
+                    onChange={(e) => setProductNumber(e.target.value)}
+                    placeholder="#12345678"
+                    className="bg-[#F7F7F8] p-2 rounded-[8px]"
+                    type="text"
+                  />
+                </div>
+
                 <p className="text-sm font-semibold">Бүтээгдэхүүний зураг</p>
                 <div className="flex gap-6 justify-between">
                   <label
@@ -184,7 +201,7 @@ export const Products = () => {
                     <input
                       hidden
                       type="file"
-                      className=""
+                      name="image"
                       id="avatar"
                       onChange={(e) => {
                         setImages(e.target.files[0]);
@@ -198,7 +215,7 @@ export const Products = () => {
                       type="file"
                       className=""
                       onChange={(e) => {
-                        setImages(e.target.files[0]);
+                        // setImages(e.target.files[0]);
                       }}
                     />
                   </label>
@@ -209,19 +226,16 @@ export const Products = () => {
                       type="file"
                       className=""
                       onChange={(e) => {
-                        setImages(e.target.files[0]);
+                        // setImages(e.target.files[0]);
                       }}
                     />
                   </label>
-                  <button>
-                    <img src="assets/icons/add.svg" alt="" />
-                  </button>
                 </div>
               </div>
             </div>
 
             <div className="p-6 bg-white rounded-[12px] flex gap-4">
-              <div className="flex flex-col w-1/2 gap-2">
+              <div className="flex flex-col w-full gap-2">
                 <p className="text-sm font-semibold">Үндсэн үнэ</p>
                 {update ? (
                   <input
@@ -239,84 +253,35 @@ export const Products = () => {
                   />
                 )}
               </div>
-
-              <div className="flex flex-col w-1/2 gap-2">
-                <p className="text-sm font-semibold">Үлдэгдэл тоо ширхэг</p>
-                {update ? (
-                  <input
-                    onChange={(e) => setQty(e.target.value)}
-                    defaultValue={product.qty}
-                    className="bg-[#F7F7F8] p-2 rounded-[8px]"
-                    type="text"
-                  />
-                ) : (
-                  <input
-                    onChange={(e) => setQty(e.target.value)}
-                    placeholder="Үлдэгдэл тоо ширхэг"
-                    className="bg-[#F7F7F8] p-2 rounded-[8px]"
-                    type="text"
-                  />
-                )}
-              </div>
             </div>
-          </div>
 
-          <div className="w-1/2 flex flex-col gap-6">
             <div className=" flex flex-col p-6 gap-4 bg-white rounded-lg">
               <div className="flex flex-col gap-2">
-                <p className="text-sm font-semibold">Ерөнхий ангилал</p>
-                <input
-                  className="h-14 bg-[#F7F7F8] rounded-[8px] p-2"
-                  placeholder="Сонгох"
-                  type="text"
-                />
+                <p className="text-sm font-semibold">Brand</p>
+                <select
+                  className="border-[1px] bg-[#F7F7F8] p-2 rounded-[8px]"
+                  name=""
+                  id="">
+                  <option value=""></option>
+                  <option value="">LV</option>
+                  <option value="">Hermes</option>
+                  <option value="">Burberry</option>
+                  <option value="">Dior</option>
+                </select>
               </div>
               <div className="flex flex-col gap-2">
-                <p className="text-sm font-semibold">Дэд ангилал</p>
-                <input
-                  className="h-14 bg-[#F7F7F8] rounded-[8px] p-2"
-                  placeholder="Сонгох"
-                  type="text"
-                />
+                <p className="text-sm font-semibold">Aнгилал</p>
+                <select
+                  className="border-[1px] bg-[#F7F7F8] p-2 rounded-[8px]"
+                  name=""
+                  id="">
+                  <option value="P"></option>
+                  <option value="">Hand Bag</option>
+                  <option value="">Travel Bag</option>
+                  <option value="">Back Bag</option>
+                  <option value="">Assessory Bag</option>
+                </select>
               </div>
-            </div>
-
-            <div className="p-6 flex flex-col gap-6 rounded-[12px] bg-white">
-              <p className="text-lg font-semibold">Төрөл</p>
-
-              <div className="flex flex-col gap-2">
-                <div className="flex gap-2">
-                  <p>Өнгө</p>
-                  <button className="pl-[25px]">
-                    <img src="assets/icons/add.svg" alt="" />
-                  </button>
-                </div>
-
-                <div className="flex gap-2">
-                  <p>Хэмжээ</p>
-                  <button className="pl-[3px]">
-                    <img src="assets/icons/add.svg" alt="" />
-                  </button>
-                </div>
-              </div>
-
-              <button className="border-[1px] border-[#D6D8DB] h-9 w-[118px] rounded-[8px] font-semibold">
-                Төрөл нэмэх
-              </button>
-            </div>
-
-            <div className="p-6 flex flex-col gap-1 bg-white h-[184px] rounded-[12px]">
-              <p className="font-semibold">Таг</p>
-
-              <input
-                placeholder="Таг нэмэх"
-                className="bg-[#F7F7F8] p-2 h-[58px] rounded-[8px]"
-                type="text"
-              />
-
-              <p className="text-sm text-[#5E6166]">
-                Санал болгох: Гутал , Цүнх , Эмэгтэй
-              </p>
             </div>
           </div>
         </div>
