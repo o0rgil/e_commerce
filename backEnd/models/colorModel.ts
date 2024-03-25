3728d813ffbce9f0edb8dc2d217b1e859fad2cf8
battsetsegbatbaatar <147494554+battsetsegbatbaatar@users.noreply.github.com>
2024-03-25 19:47:03 +0800

    created back-end

--- backEnd/models/colorModel.ts
+++ backEnd/models/colorModel.ts
@@ -6,6 +6,8 @@ import { Schema, model } from "mongoose";
 const ColorSchema: Schema = new Schema({
   color: { type: String, required: true },
   bagId: { type: Schema.Types.ObjectId, ref: "Bag", required: true },
+  bagCode: { type: String, required: true, unique: true },
+  images: { type: [], required: true },
   CreatedAt: { type: Date, default: new Date() },
   UpdatedAt: { type: Date, default: new Date() },
 });
